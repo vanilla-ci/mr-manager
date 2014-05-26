f956d3d91ff9855c8721e68092f7cba5444efc62
Devon Moss <dvnmss@gmail.com>
2014-05-26 15:54:10 -0600

    adding postBuild steps to the work model bean

--- src/main/java/com/vanillaci/model/Work.java
+++ src/main/java/com/vanillaci/model/Work.java
@@ -13,16 +13,19 @@ public class Work {
 	@NotNull private final String id;
 	@NotNull private final Map<String, String> parameters;
 	@NotNull private final List<ScriptMessage> scripts;
+	@NotNull private final List<ScriptMessage> postBuildScripts;
 
 
 	public Work(
 		@JsonProperty("id") @NotNull String id,
 		@JsonProperty("parameters") @NotNull Map<String, String> parameters,
-		@JsonProperty("scripts") @NotNull List<ScriptMessage> scripts
+		@JsonProperty("scripts") @NotNull List<ScriptMessage> scripts,
+		@JsonProperty("postBuildScripts") @NotNull List<ScriptMessage> postBuildScripts
 	) {
 		this.id = id;
 		this.parameters = ImmutableMap.copyOf(parameters);
 		this.scripts = ImmutableList.copyOf(scripts);
+		this.postBuildScripts = postBuildScripts;
 	}
 
 	@NotNull
@@ -38,5 +41,10 @@ public class Work {
 	@NotNull
 	public List<ScriptMessage> getScripts() {
 		return scripts;
+	}
+
+	@NotNull
+	public List<ScriptMessage> getPostBuildScripts() {
+		return postBuildScripts;
 	}
 }
